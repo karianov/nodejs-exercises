8396e5a600e7ebf4d3250357672be55cdc045e51
karianov <karianov@correo.udistrital.edu.co>
2020-09-14 22:13:42 -0500

    Functions imports

--- b-notes-app/app.js
+++ b-notes-app/app.js
@@ -1,9 +1,3 @@
-// Basic own imports
-// const add = require('./utils.js');
-// const sum = add(4, -2);
-// console.log(sum);
-
-// Functions imports
 const getNotes = require('./notes.js');
 
 const notes = getNotes();
