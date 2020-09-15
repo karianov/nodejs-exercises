fe548004f1c3adab24e85ca767fbaa3d7db044c9
karianov <karianov@correo.udistrital.edu.co>
2020-09-15 18:45:47 -0500

    NPM init and use of validator NPM package

--- b-notes-app/app.js
+++ b-notes-app/app.js
@@ -1,5 +1,9 @@
 const getNotes = require('./notes.js');
+const validator = require('validator')
 
 const notes = getNotes();
 
 console.log(notes);
+
+console.log(validator.isEmail('kevin@example'));
+console.log(validator.isURL('https:/github.com'));
