8dcf954fdcbf3c5612a1b741d280e9e405bbcee8
karianov <karianov@correo.udistrital.edu.co>
2020-09-15 19:13:25 -0500

    Use of Chalk package for logs styling

--- b-notes-app/app.js
+++ b-notes-app/app.js
@@ -1,9 +1,16 @@
 const getNotes = require('./notes.js');
 const validator = require('validator')
+const chalk = require('chalk');
 
 const notes = getNotes();
 
 console.log(notes);
 
+// Validator examples
 console.log(validator.isEmail('kevin@example'));
 console.log(validator.isURL('https:/github.com'));
+
+// Chalk examples
+console.log(chalk.green('Success!'));
+console.log(chalk.bold('Bold!'));
+console.log(chalk.inverse.green('Inversed!'));
