12aa8fe14dc424d3be69ca05ba65ba9e5a6a1c93
karianov <karianov@correo.udistrital.edu.co>
2020-09-16 23:21:04 -0500

    Get, set and store data with JSON

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+// Example
+
 // const book = {
 //   title: 'Men are from mars and women are from venus',
 //   author: 'John Gray',
@@ -8,7 +10,15 @@ const fs = require('fs');
 // const bookJSON = JSON.stringify(book);
 // fs.writeFileSync('1-json.json', bookJSON);
 
-const dataBuffer = fs.readFileSync('1-json.json')
-const dataText = dataBuffer.toString();
-const data = JSON.parse(dataText);
-console.log(data);
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataText = dataBuffer.toString();
+// const data = JSON.parse(dataText);
+// console.log(data);
+
+const userBuffer = fs.readFileSync('1-json.json');
+const userText = userBuffer.toString();
+const user = JSON.parse(userText);
+user.name = 'Kevin';
+user.age = 25;
+const userData = JSON.stringify(user);
+fs.writeFileSync('1-json.json', userData);
