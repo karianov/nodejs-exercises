d31512a8f7cf8463ba154fa54479d2bd206bd060
karianov <karianov@correo.udistrital.edu.co>
2020-09-16 20:52:23 -0500

    Argument parsing with yargs: create commands

--- b-notes-app/app.js
+++ b-notes-app/app.js
@@ -1,13 +1,47 @@
 const getNotes = require('./notes.js');
-const validator = require('validator')
+const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-const command = process.argv[2];
+// Set yargs version
+yargs.version('1.1.0');
 
-console.log(process.argv);
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: () => {
+    console.log('Adding a new note');
+  },
+});
 
-if (command === 'add') {
-  console.log('Adding note');
-} else if (command === 'remove') {
-  console.log('Removing note');
-}
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove an existing note',
+  handler: () => {
+    console.log('Removing the note');
+  },
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List all existing notes',
+  handler: () => {
+    console.log('Listing out the notes');
+  },
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => {
+    console.log('Reading the note');
+  },
+});
+
+// Operations: add, remove, read, list
+
+console.log(yargs.argv);
