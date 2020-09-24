91187ea117d707ceb5a9245ddd75711fa41d5576
karianov <karianov@correo.udistrital.edu.co>
2020-09-23 21:15:28 -0500

    Weather app init and HTTP request example

--- c-weather-app/app.js
+++ c-weather-app/app.js
@@ -1,11 +1,15 @@
-console.log('Starting');
+const request = require('postman-request');
 
-setTimeout(() => {
-  console.log('2 second timer');
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=7be3e52a938d826432fcae3d8332ea80&query=Bogota';
 
-setTimeout(() => {
-  console.log('0 second timer');
-}, 0);
-
-console.log('Stopping');
+request(url, (error, response, body) => {
+  // Response
+  const weatherData = JSON.parse(response.body);
+  console.log(weatherData.current);
+  // Error (If OK then error is null)
+  console.warn(error);
+  // Body is response.body
+  console.log();
+  console.log(body);
+  console.log();
+});
