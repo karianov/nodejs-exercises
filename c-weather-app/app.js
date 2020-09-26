95983a60c5508dbbb52d36d1d41ec146ac8dd4a2
karianov <karianov@correo.udistrital.edu.co>
2020-09-25 21:11:09 -0500

    API parameters, JSON format to response body and response options management

--- c-weather-app/app.js
+++ c-weather-app/app.js
@@ -1,15 +1,15 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=7be3e52a938d826432fcae3d8332ea80&query=Bogota';
+const url = 'http://api.weatherstack.com/current?access_key=7be3e52a938d826432fcae3d8332ea80&query=Bogota&units=m';
 
-request(url, (error, response, body) => {
-  // Response
-  const weatherData = JSON.parse(response.body);
-  console.log(weatherData.current);
-  // Error (If OK then error is null)
-  console.warn(error);
-  // Body is response.body
-  console.log();
-  console.log(body);
-  console.log();
+request({ url: url, json: true }, (error, response, body) => {
+  const weatherMeasure = response.body.current;
+  console.log(
+    weatherMeasure.weather_descriptions[0] +
+      '. It is currently ' +
+      weatherMeasure.temperature +
+      ' degrees out. It feels like ' +
+      weatherMeasure.feelslike +
+      ' degrees out.'
+  );
 });
