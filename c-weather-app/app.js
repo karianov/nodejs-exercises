05eec737a533b933b05a917c2b1dc4c9b5c19ee7
karianov <karianov@correo.udistrital.edu.co>
2020-09-28 22:43:34 -0500

    HTTP request for place coordinates with mapbox

--- c-weather-app/app.js
+++ c-weather-app/app.js
@@ -1,8 +1,9 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=7be3e52a938d826432fcae3d8332ea80&query=Bogota&units=m';
+const weatherStackUrl =
+  'http://api.weatherstack.com/current?access_key=7be3e52a938d826432fcae3d8332ea80&query=Bogota&units=m';
 
-request({ url: url, json: true }, (error, response, body) => {
+request({ url: weatherStackUrl, json: true }, (error, response, body) => {
   const weatherMeasure = response.body.current;
   console.log(
     weatherMeasure.weather_descriptions[0] +
@@ -12,4 +13,18 @@ request({ url: url, json: true }, (error, response, body) => {
       weatherMeasure.feelslike +
       ' degrees out.'
   );
+});
+
+// HTTP request challenge
+const placeName = 'Los Angeles';
+const mapboxUrl =
+  'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+  placeName +
+  '.json?access_token=pk.eyJ1Ijoia2V2aW5yaWFubyIsImEiOiJja2ZuN2Vyd2UxcTI0MzFwbTh1NjJjejN2In0.SPtK8iZqQJ0ZtjwIzyFM2A&limit=1';
+
+request({ url: mapboxUrl, json: true }, (error, response, body) => {
+  const place = response.body.features[0];
+  console.log(place.place_name + '.');
+  console.log('Longitude: ' + place.center[0]);
+  console.log('Latitude: ' + place.center[1]);
 });
